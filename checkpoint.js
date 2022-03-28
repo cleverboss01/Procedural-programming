f447961a13fe12e29c42b9a4b03567a601f32dc8
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-03-28 16:03:47 +0100

    final procedural checkpoint

--- checkpoint.js
+++ checkpoint.js
@@ -11,25 +11,26 @@
 // Description:
 //1. Write a procedure, called dot_product which calculates in the variable ps, the dot(scalar) product of v1 and v2 (v1 and v2 are vectors of IR)
 
-function dotProduct (v1, v2) {
-    var ps = v1 * v2;
-    return ps;
-}
+let vectorsArr = [[7, 1, 1],[6, 0, 2],[0,0,1], [5, 7, 0], [3, 1, 2], [2, 3 ,8], [0,0,0]];
 
+const dotProduct = (vector1, vector2) =>{
+    let ps = 0;
+    for(let i = 0; i < vector1.length; i++){
+        ps += vector1[i] * vector2[i]
+      }
+    return ps
+  }
+  
 //2. Write an algorithm which determines, for n pairs of given vectors, whether two vectors of given IR are orthogonal, by calling the procedure defined in the previous question. The dot product of two orthogonal vectors is zero.
 
-const v1 = [1,2,3,4,5];
-const v2 = [3,4,5,2,7];
-
-const orthogonal = v1.filter( n => {
-    for(i=0; i<v1.length; i++){
-        for (j=0; j<v2.length; j++){
-            if(n == v2[j]){
-                return true;
-            }
+const orthogonal = vectors => {
+    for(let i = 0; i < vectors.length; i++){
+        for(let j = i + 1; j < vectors.length; j++){
+          if(dotProduct(vectors[i], vectors[j]) === 0){
+              console.log(`${vectors[i]} and ${vectors[j]} are orthogonal `)
+          }
         }
-    }
-});
+      }    
+}
 
-// 3. Modify the previous algorithm if you use a dot_product function instead of a procedure.
-
+orthogonal(vectorsArr);
